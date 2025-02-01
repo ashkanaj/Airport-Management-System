f04465d14a4aa458fc20c9fba75eaf41bf14a733
Ashkan Jafarinejad <ashkanaj2019@gmail.com>
2025-02-01 22:15:30 +0330

    Airport management

--- model/entity/airplane_employees.py
+++ model/entity/airplane_employees.py
@@ -1,2 +1,7 @@
 class AirplaneEmployee:
-    pass
+    def __init__(self, airport_name, city_name, area, id=None):
+        self.airport_name = airport_name
+        self.city_name = city_name
+        self.area = area
+        self.id = id
+
