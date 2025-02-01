8ffd18d8f99a1d2b584153d7e8c7c8e1c113d53e
Ashkan Jafarinejad <ashkanaj2019@gmail.com>
2025-02-01 22:33:15 +0330

    Airport management

--- model/entity/ticket_counter.py
+++ model/entity/ticket_counter.py
@@ -1,2 +1,16 @@
 class TicketCounter:
-    pass
+    def __init__(self, ticket_id, passenger_id, source, destination, price, flight_id):
+        self.ticket_id = ticket_id
+        self.passenger_id = passenger_id
+        self.source = source
+        self.destination = destination
+        self.price = price
+        self.flight_id = flight_id
+
+    def ticket_details(self):
+        pass
+
+    def book_ticket(self):
+        pass
+
+
