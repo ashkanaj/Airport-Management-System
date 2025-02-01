caa3963677305c37e696b81ac33cb7d1933bff8e
Ashkan Jafarinejad <ashkanaj2019@gmail.com>
2025-02-01 22:30:56 +0330

    Airport management

--- model/entity/airport_management.py
+++ model/entity/airport_management.py
@@ -1,2 +1,11 @@
-class AirportManagement:
-    pass
+from model.entity.employees import Employee
+
+
+class AirportManagement(Employee):
+    def __init__(self, employee_id, employee_name, employee_salary, designation):
+        super().__init__(employee_id, employee_name, employee_salary)
+        self.designation = designation
+
+    def employee_details(self):
+        parent_details = super().employee_details()
+
