2cb533d8f7b7dad0607f60637104df9b1088ea76
Ashkan Jafarinejad <ashkanaj2019@gmail.com>
2025-02-01 22:29:16 +0330

    Airport management

--- model/entity/airport_employees.py
+++ model/entity/airport_employees.py
@@ -2,7 +2,13 @@ from model.entity.employees import Employee
 
 
 class AirportEmployees(Employee):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,employee_id,employee_name,employee_salary,designation,department):
+        super().__init__(employee_id,employee_name,employee_salary)
+        self.designation = designation
+        self.department = department
+        
+        
+    def employee_details(self):
+        parent_details=super().employee_details()
 
 
