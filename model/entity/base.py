ef40328ec941416e438ea59771ce5e79d64868a9
ali jafarinejad <ashkanaj2019@gmail.com>
2025-02-08 20:18:21 +0330

    Airport management

--- model/entity/base.py
+++ model/entity/base.py
@@ -1,2 +1,5 @@
-from model  import *
+from sqlalchemy.orm import DeclarativeBase
 
+class Base(DeclarativeBase):
+    def __repr__(self):
+        return str({c.name: getattr(self,c.name) for c in self.__table__.columns})
